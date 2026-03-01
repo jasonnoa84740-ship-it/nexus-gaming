9311991943bad67156dc3cf5cbaeb7305b7d475a
Your Name sanchez <you@example.com jasonnoa84740@gmail.com>
2026-03-01 21:12:12 +0100

    Remove CartProvider from providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,17 +3,14 @@
 import { ThemeProvider } from "@/components/ThemeProvider";
 import Cursor from "@/components/Cursor";
 import PageTransition from "@/components/PageTransition";
-import { CartProvider } from "@/lib/cart";
 import NexusBackground from "@/components/NexusBackground";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <CartProvider>
       <ThemeProvider>
         <NexusBackground />
         <Cursor />
         <PageTransition>{children}</PageTransition>
       </ThemeProvider>
-    </CartProvider>
   );
 }
