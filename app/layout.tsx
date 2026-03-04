d4f57bcd3ee64a2a0476ec1e274cf2b3d1c6239d
Your Name sanchez <you@example.com jasonnoa84740@gmail.com>
2026-03-04 22:24:49 +0100

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,38 @@
+import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const metadata: Metadata = {
+  title: "Nexus Gaming",
+  description: "Sélection gaming Nexus avec liens Amazon",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="fr">
       <body>
-         {children}
-          <div className="nx-watermark" />
+
+        {children}
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-F3BLLTBHYK"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-F3BLLTBHYK');
+          `}
+        </Script>
+
       </body>
     </html>
   );
